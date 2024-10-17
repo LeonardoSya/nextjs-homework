77cb3585124e362a46ae6740d1c2f95112b3375c
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-10-18 00:27:36 +0800

    second commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LocationAggregatorMap from '../components/Map'
+
+const HomePage = () => {
+  return (
+    <div className="relative min-h-screen">
+      <LocationAggregatorMap />
+    </div>
+  )
+}
+
+export default HomePage
--- components/map.tsx
+++ components/map.tsx
@@ -6,9 +6,29 @@ import { HexagonLayer } from '@deck.gl/aggregation-layers'
 import DeckGL from '@deck.gl/react'
 import "mapbox-gl/dist/mapbox-gl.css"
 
+import {
+  lightingEffect,
+  material,
+  INITIAL_VIEW_STATE,
+  colorRange,
+} from "../lib/mapconfig.js";
+
 const LocationAggregatorMap = () => {
   return (
-    <div>Map</div>
+    <div>
+    <DeckGL
+      effects={[lightingEffect]}
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+    >
+      <Map
+        className=""
+        controller={true}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        mapStyle="mapbox://styles/petherem/cl2hdvc6r003114n2jgmmdr24"
+      ></Map>
+    </DeckGL>
+  </div>  
   )
 }
 
